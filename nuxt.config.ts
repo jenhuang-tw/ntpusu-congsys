e9d29e5d7717e0d12cb5f1b3c59a22f3c5dfbddd
Zhen HUANG <me@zhenhuang.tw>
2025-07-07 22:09:28 +0800

    取消使用 eslint 模組

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   modules: [
-    '@nuxt/eslint',
     '@nuxtjs/tailwindcss',
     '@nuxtjs/color-mode'
   ],
